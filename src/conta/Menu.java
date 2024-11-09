8b5e863fe277b96511186111f8f748ba3e3b80ab
Seu Nome <seuemail@exemplo.com>
2024-11-08 22:19:27 -0300

    Herança, Classe abstrata e Interface

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -3,12 +3,37 @@ package conta;
 import java.util.Scanner;
 
 import conta.model.Conta;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
 import conta.util.Cores;
 
 public class Menu {
 	public static void main(String[] args) {
-		
-		
+
+		// Teste da Classe Conta
+		Conta c1 = new Conta(3, 123, 1, "Mariana", 500000.0f);
+		c1.visualizar();
+		c1.sacar(12000.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
+		c1.visualizar();
+
+		// Teste da Classe Conta Corrente
+		ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "José da Silva", 0.0f, 1000.0f);
+		cc1.visualizar();
+		cc1.sacar(12000.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
+
+		// Teste da Classe Conta Poupança
+		ContaPoupanca cp1 = new ContaPoupanca(2, 123, 2, "Maria dos Santos", 100000.0f, 15);
+		cp1.visualizar();
+		cp1.sacar(1000.0f);
+		cp1.visualizar();
+		cp1.depositar(5000.0f);
+		cp1.visualizar();
+
 		Scanner sc = new Scanner(System.in);
 
 		int opcao;
@@ -40,50 +65,50 @@ public class Menu {
 			if (opcao == 9) {
 				System.out.println("\nBanco do Brazil com Z - O seu Futuro começa aqui!");
 				sobre();
-                 sc.close();
+				sc.close();
 				System.exit(0);
 			}
 
 			switch (opcao) {
-				case 1:
-					System.out.println(Cores.TEXT_PURPLE_BOLD_BRIGHT + "Criar Conta\n\n");
+			case 1:
+				System.out.println(Cores.TEXT_PURPLE_BOLD_BRIGHT + "Criar Conta\n\n");
 
-					break;
-				case 2:
-					System.out.println(Cores.TEXT_PURPLE_BOLD_BRIGHT + "Listar todas as Contas\n\n");
+				break;
+			case 2:
+				System.out.println(Cores.TEXT_PURPLE_BOLD_BRIGHT + "Listar todas as Contas\n\n");
 
-					break;
-				case 3:
-					System.out.println(Cores.TEXT_PURPLE_BOLD_BRIGHT + "Consultar dados da Conta - por número\n\n");
+				break;
+			case 3:
+				System.out.println(Cores.TEXT_PURPLE_BOLD_BRIGHT + "Consultar dados da Conta - por número\n\n");
 
-					break;
-				case 4:
-					System.out.println(Cores.TEXT_PURPLE_BOLD_BRIGHT + "Atualizar dados da Conta\n\n");
+				break;
+			case 4:
+				System.out.println(Cores.TEXT_PURPLE_BOLD_BRIGHT + "Atualizar dados da Conta\n\n");
 
-					break;
-				case 5:
-					System.out.println(Cores.TEXT_PURPLE_BOLD_BRIGHT + "Apagar a Conta\n\n");
+				break;
+			case 5:
+				System.out.println(Cores.TEXT_PURPLE_BOLD_BRIGHT + "Apagar a Conta\n\n");
 
-					break;
-				case 6:
-					System.out.println(Cores.TEXT_PURPLE_BOLD_BRIGHT + "Saque\n\n");
+				break;
+			case 6:
+				System.out.println(Cores.TEXT_PURPLE_BOLD_BRIGHT + "Saque\n\n");
 
-					break;
-				case 7:
-					System.out.println(Cores.TEXT_PURPLE_BOLD_BRIGHT + "Depósito\n\n");
+				break;
+			case 7:
+				System.out.println(Cores.TEXT_PURPLE_BOLD_BRIGHT + "Depósito\n\n");
 
-					break;
-				case 8:
-					System.out.println(Cores.TEXT_PURPLE_BOLD_BRIGHT + "Transferência entre Contas\n\n");
+				break;
+			case 8:
+				System.out.println(Cores.TEXT_PURPLE_BOLD_BRIGHT + "Transferência entre Contas\n\n");
 
-					break;
-				default:
-					System.out.println(Cores.TEXT_RED_BOLD+ "\nOpção Inválida!\n");
-					break;
+				break;
+			default:
+				System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n");
+				break;
 			}
 		}
 	}
-    
+
 	public static void sobre() {
 		System.out.println("\n*********************************************************");
 		System.out.println("Projeto Desenvolvido por: Daffne Vieira Rodrigues");
